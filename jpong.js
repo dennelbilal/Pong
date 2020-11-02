6719e747edd85ec2943de163ce6a5f26adfb8194
dennelbilal <72392273+dennelbilal@users.noreply.github.com>
2020-11-02 16:12:17 +0100

    pong

--- jpong.js
+++ jpong.js
@@ -5,9 +5,10 @@ let gauche=parseInt ($("#balle").css("left"));
 let haut=parseInt ($("#balle").css("top"));
 alert(gauche);
 
-setInterval(funtion(){
+setInterval(function(){
     gauche=gauche+1;
-    haut= haut+0.5;
+    haut=haut+0.5;
     $("#balle").css("left",gauche);
     $("#balle").css("top",haut);
-}, 10;
+
+}, 10);
