350f18e1451451f539a94b7bdfd660f52b216f79
dennelbilal <72392273+dennelbilal@users.noreply.github.com>
2020-11-03 18:48:59 +0100

    pong

---
+++ js/pong.js
@@ -0,0 +1,20 @@
+
+
+let largeur=$("#ball").width();
+let gauche=parseInt(($("#ball").css("left")));
+let haut=parseInt(($("#ball").css("top")));
+alert(gauche);
+
+let i=0;
+setInterval(function(){
+    gauche = gauche + 1;
+    haut = haut + 1;
+    $("#ball").css("left",gauche);
+    $("#ball").css("top",haut)
+    if(gauche >= 800){
+        alert("elle est dehors")
+    }
+    if (haut >= 400){
+        alert("elle est dehors")
+    }
+}, 10);
