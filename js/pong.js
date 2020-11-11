4a3c4c0dd8a96f82b75a735bd6410305cadd80f8
dennelbilal <72392273+dennelbilal@users.noreply.github.com>
2020-11-11 17:36:22 +0100

    pong

--- js/pong.js
+++ js/pong.js
@@ -4,44 +4,54 @@
 //let gauche=parseInt($("#balle").css("left"));
 //let haut=parseInt($("#balle").css("top"));
 
-//fichier Terrain.js
+//fichier Terrain
 class Terrain{
     constructor($element){
         this.$element=$element
-        this.Largeur=$element.width();
+        this.largeur=$element.width();
         this.hauteur=$element.height();
-    }     
+            
+        }
+    
+    }
 
-}
-//fichier Balle.js
+//Classe Balle
+
 class Balle{
     constructor($html){
         this.$html=$html;
-        this.gauche=parseInt($("#balle").css("left"));
         this.haut=parseInt($("#balle").css("top"));
+        this.gauche=parseInt($("#balle").css("left"));
         this.vitesseX=2;
         this.vitesseY=0.5;
-    }     
+        
+    }
     majHTML(){
-       this.$html.css("left",balle.gauche);
-       this.$html.css("top",balle.haut);
+    this.$html.css("left",balle.gauche);
+    this.$html.css("top",balle.haut);
     }
-}
- let terrain=new Terrain($("#terrain"));
- let balle=new Balle($("#balle"));
 
-//modifie la position de la balle
+}
+
+let terrain=new Terrain($("#terrain"));
+let balle=new Balle($("#balle"));
+
+
+// Script de la balle
+
 setInterval(function(){
-    balle.gauche =balle.gauche + balle.vitesseX;
+    balle.gauche = balle.gauche + balle.vitesseX;
     balle.haut = balle.haut + balle.vitesseY;
-    
-    if(balle.gauche>terrain.Largeur){
-        balle.gauche=terrain.Largeur;
-        balle.vitesseX=balle.vitesseX*-1;
-    }
+
+
+    if(balle.gauche>terrain.largeur){
+    balle.gauche=terrain.largeur;
+    balle.vitesseX=balle.vitesseX*-1;
+}
+
     if(balle.haut>terrain.hauteur){
-        balle.haut=0;
-    }
-    balle.majHTML
-    
+    balle.haut=0;
+}
+balle.majHTML();
+
 }, 10);
